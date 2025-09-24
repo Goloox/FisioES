bae71574a37c00f697a118715a9c2efd813b2ddc
Goloox <157188592+Goloox@users.noreply.github.com>
2025-09-23 19:09:33 -0600

    Create ping.js

---
+++ netlify/functions/ping.js
@@ -0,0 +1,7 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({ ok: true, ts: Date.now() })
+  };
+};
